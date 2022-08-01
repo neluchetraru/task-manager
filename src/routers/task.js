0825ba2531d23acc29a67a65cd673410a117ef1e
Chetraru Ion <neluchetraru@gmail.com>
2022-08-01 10:49:26 +0200

    [1] GET tasks added queries: filters, sorting, pagination

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,9 +16,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt:asc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if (req.query.completed){ 
+        match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'asc' ? 1 : -1
+    }
     try {
-        await req.user.populate('tasks')
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        })
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
