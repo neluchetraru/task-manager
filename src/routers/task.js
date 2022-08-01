3ca467a0fb1bf1ff19db0eecbc85ea96c9455279
Chetraru Ion <neluchetraru@gmail.com>
2022-08-01 10:06:39 +0200

    [1] REST APIs \n[2] API Authentication and security

---
+++ src/routers/task.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+const auth = require('../middleware/auth')
+
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.get('/tasks', auth, async (req, res) => {
+    try {
+        await req.user.populate('tasks')
+        res.send(req.user.tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/tasks/:id', auth, async (req, res) => {
+    const _id = req.params.id
+    try {
+        // const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id, owner: req.user._id})
+        
+        if (!task) {
+            return res.status(404).send()
+        }
+        
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/tasks/:id', auth, async (req, res) => {
+    const allowedUpdates = ['description', 'completed']
+    const updates = Object.keys(req.body)
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({
+            error: 'Invalid updates!'
+        })
+    }
+
+    try {
+        const task = await Task.findOne({ _id:req.params.id, owner: req.user._id })
+        // const task = task.findById(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        res.send(task)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.delete('/tasks/:id', auth, async (req, res) => {
+    try {
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
